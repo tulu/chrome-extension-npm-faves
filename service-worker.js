3317d81b2c3866851ccb3ccee0fddf8dcb388482
Ruben <rsosenke@gmail.com>
2021-05-31 09:53:35 -0300

    Renaming of service worker and background script

--- service-worker.js
+++ service-worker.js
@@ -10,7 +10,7 @@ try {
     "/scripts/utils/npmFaves-helpers.js",
     "/scripts/services/npmFaves-registry-service.js",
     "/scripts/services/npmFaves-storage-service.js",
-    "/scripts/extension.js" // <-- Main file.
+    "/scripts/background.js" // <-- Main file.
   );
 } catch (e) {
   console.error(e);
